cd8c8f6c26891722017efc69c659d0169f357db1
DESKTOP-AQP88U3\paulo <pauloandre232@gmail.com>
2022-09-19 22:35:51 -0300

    Adds update, delete and list to productController

--- src/web/controllers/ProductController.ts
+++ src/web/controllers/ProductController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
 import CreateProductService from "../../services/CreateProductService";
 import { container } from "tsyringe";
+import ListProductService from "../../services/ListProductService";
+import UpdateProductService from "../../services/UpdateProductService";
+import DeleteProductService from "../../services/DeleteProductService";
 
 export default class ProductController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listProductsService = container.resolve(ListProductService);
+    const products = await listProductsService.execute();
+
+    return response.json(products);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { title, price, description, category } = request.body;
     const createProductService = container.resolve(CreateProductService);
@@ -14,5 +24,30 @@ export default class ProductController {
     });
 
     return response.json(product);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { title, price, category, description } = request.body;
+    const { id } = request.params;
+
+    const updateProductService = container.resolve(UpdateProductService);
+    const product = await updateProductService.execute({
+      id,
+      title,
+      price,
+      description,
+      category
+    });
+
+    return response.json(product);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteProductService = container.resolve(DeleteProductService);
+    await deleteProductService.execute({ id });
+
+    return response.json([]);
   }
 }
